d0ec2e7be8b64b2eccb2c8783b8dbde394db3957
Joaquim <joaquimventuradasilva@gmail.com>
2022-03-15 16:35:34 -0300

    Improvement in project organization

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -1,21 +1,65 @@
 package com.example.marvelapp.presentation.characters
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.marvelapp.R
+import androidx.fragment.app.Fragment
+import com.example.core.domain.model.Character
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!! //get() - Instanciado no momento que é chamado
+
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2021/11/11/11150912834388.jpg?ims=704x264"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2021/11/11/11150912834388.jpg?ims=704x264"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2021/11/11/11150912834388.jpg?ims=704x264"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2021/11/11/11150912834388.jpg?ims=704x264"
+                )
+            )
+        )
+
+    }
+
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters) {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
     }
 
 }
