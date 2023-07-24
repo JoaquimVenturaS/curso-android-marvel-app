5c8012a42287f9b30f91a9c848d125d9c65cbbd4
Joaquim Ventura <Joaquimventuradasilva@gmail.com>
2023-07-24 13:40:10 -0300

    Adicionado testes unitarios do CharactersViewModel e um modulo especifico so para testes

--- app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -1,14 +1,18 @@
 package com.example.marvelapp.presentation.characters
 
 import androidx.paging.PagingData
-import com.example.core.domain.model.Character
 import com.example.core.useCase.GetCharactersUseCase
+import com.example.testing.MainCoroutineRule
+import com.example.testing.model.CharacterFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -18,12 +22,22 @@ import org.mockito.junit.MockitoJUnitRunner
 class CharactersViewModelTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     @Mock
     lateinit var getCharactersUseCase: GetCharactersUseCase
 
     private lateinit var charactersViewModel: CharactersViewModel
+
+    private val charactersFactory = CharacterFactory()
+
+    private val pagingCharacters = PagingData.from(
+        listOf(
+            charactersFactory.create(CharacterFactory.Hero.ThreeDMan),
+            charactersFactory.create(CharacterFactory.Hero.ABomb)
+        )
+    )
 
     @Before
     fun setUp() {
@@ -34,15 +48,25 @@ class CharactersViewModelTest {
     @Test
     fun `should validate the paging data object values when calling charactersPagingData`() =
         runBlockingTest {
-            val expectedPagingData = PagingData.from(
-                listOf(
-                    Character(
-
-                    )
+            whenever(getCharactersUseCase.invoke(any())).thenReturn(
+                flowOf(
+                    pagingCharacters
                 )
             )
 
-            val result = charactersViewModel.charactersPagingData("").first()
+            val result = charactersViewModel.charactersPagingData("")
+
+            assertEquals(1, result.count())
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`() =
+        runBlockingTest {
+            whenever(getCharactersUseCase.invoke(any()))
+                .thenThrow(RuntimeException())
+
+            charactersViewModel.charactersPagingData("")
         }
 
 }
--- core/src/main/java/com/example/core/useCase/GetCharactersUseCase.kt
+++ core/src/main/java/com/example/core/useCase/GetCharactersUseCase.kt
@@ -5,13 +5,21 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.core.data.repository.CharactersRepository
 import com.example.core.domain.model.Character
+import com.example.core.useCase.GetCharactersUseCase.GetCharactersParams
 import com.example.core.useCase.base.PagingUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCharactersUseCase @Inject constructor(
+interface GetCharactersUseCase {
+    operator fun invoke(params: GetCharactersParams): Flow<PagingData<Character>>
+
+    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
+}
+
+class GetCharactersUseCaseImpl @Inject constructor(
     private val charactersRepository: CharactersRepository
-) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
+) : PagingUseCase<GetCharactersParams, Character>(),
+    GetCharactersUseCase {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
         return Pager(config = params.pagingConfig) {
@@ -20,5 +28,4 @@ class GetCharactersUseCase @Inject constructor(
         }.flow
     }
 
-    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
 }
